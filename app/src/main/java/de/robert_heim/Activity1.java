62755017a28cccc616e250c44de3e2e84049c567
shivam36 <shivamagarwal36@gmail.com>
2016-02-01 21:23:48 +0530

    adding files

---
+++ app/src/main/java/de/robert_heim/Activity1.java
@@ -0,0 +1,45 @@
+package de.robert_heim;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import de.robert_heim.animation.ActivitySwitcher;
+
+public class Activity1 extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity1);
+ 
+		Button switchActivityBtn = (Button) findViewById(R.id.bSwitchActivity);
+		switchActivityBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				animatedStartActivity();
+			}
+		});
+	}
+ 
+	@Override
+	protected void onResume() {
+		// animateIn this activity
+		ActivitySwitcher.animationIn(findViewById(R.id.container), getWindowManager());
+		super.onResume();
+	}
+ 
+	private void animatedStartActivity() {
+		// we only animateOut this activity here.
+		// The new activity will animateIn from its onResume() - be sure to implement it.
+		final Intent intent = new Intent(getApplicationContext(), Activity2.class);
+		// disable default animation for new intent
+		intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+		ActivitySwitcher.animationOut(findViewById(R.id.container), getWindowManager(), new ActivitySwitcher.AnimationFinishedListener() {
+			@Override
+			public void onAnimationFinished() {
+				startActivity(intent);
+			}
+		});
+	}
+}
